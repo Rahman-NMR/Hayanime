55973be843cb4127945239bf1aabd7f6fe102bf9
Rahman <49480009+Rahman-NMR@users.noreply.github.com>
2025-08-06 15:38:40 +0700

    feat: Initialize Hayanime Android project
    This commit introduces the initial project structure for the Hayanime Android application.
    
    It includes the following:
    - Basic Android project setup with Gradle build scripts.
    - Application manifest, resources (layouts, drawables, values), and navigation components.
    - Default .gitignore and Proguard rules.
    - Placeholder fragments and view models for Home, Dashboard, and Notifications screens.
    - Basic unit and instrumented tests.
    - IDE configuration files for Android Studio.

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget(JavaVersion.VERSION_21.toString())
+    }
+}
+
+android {
+    namespace = "com.animegatari.hayanime"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
+
+    defaultConfig {
+        applicationId = "com.animegatari.hayanime"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.0.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+//    testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
